02dbc32b423e58214e4b60230b91ae7709602854
Marina <lituevam@gmail.com>
2020-02-20 19:48:20 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
 exports.min = function min (array) {
-  return 0;
+    if ((arguments.length===0)|| (array.length==0)) {
+    return 0;
+    }
+  
+    let min;
+    for (let i=0; i<array.length; i++) {
+      for (let j=i+1; j<array.length;j++){
+        if (array[i]<array[j]){
+          min=array[i];
+          array[i]=array[j];
+          array[j]=min
+  
+        }
+      }
+    }
+    return  min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if ((arguments.length===0)|| (array.length===0)) {
+      return 0;
+    }
+    let max= Math.max(...array)  
+    return max ; 
 }
 
 exports.avg = function avg (array) {
-  return 0;
-}
+    if ((arguments.length===0)|| (array.length===0)) {
+      return 0;
+    }
+    let avg = [...array].reduce((a, b) => a + b, 0)/[...array].length
+    return avg
+}  
